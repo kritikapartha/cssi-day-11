73202d3b9d854c22eeef76e1736ee5debe12f748
kritikapartha <kritikaps11@gmail.com>
2021-07-26 17:42:58 +0000

    Sign in and redirect working

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -3,4 +3,14 @@ console.log(signInButton)
 
 signInButton.addEventListener("click" , () => {
     console.log("button clicked")
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user
+        console.log("login success", user)
+        window.location = "writeNote.html"
+    })
+.catch(error => {
+    console.log("login failed", error)
+})
+
 })
